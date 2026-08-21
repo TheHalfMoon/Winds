2a4325a7f2712e2792ddd814fd26af3367d88429
Abdulaziz M. Shehri <alshehriofficial@gmail.com>
2026-08-21 03:50:07 +0300

    feat(006): integrate T071 continuity store

--- src/store.rs
+++ src/store.rs
@@ -14,6 +14,8 @@ use std::path::{Path, PathBuf};
 
 #[path = "store_git_observation.rs"]
 pub(crate) mod git_observation;
+#[path = "agentic_identity.rs"]
+pub(crate) mod agentic_identity;
 
 pub type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
 
@@ -150,6 +152,7 @@ impl Store {
             "../migrations/0005_execution_git_observations.sql"
         ))?;
         connection.execute_batch(include_str!("../migrations/0006_agentic_identity.sql"))?;
+        connection.execute_batch(include_str!("../migrations/0007_agentic_session_origins.sql"))?;
         Ok(Self {
             connection,
             home: home.to_path_buf(),
