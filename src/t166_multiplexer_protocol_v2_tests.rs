1708e1a208c4582c186c3776ad3d45d8659b5180
Abdulaziz M. Shehri <alshehriofficial@gmail.com>
2026-09-21 13:11:18 +0300

    test: reject abbreviated Git OIDs in T166

--- src/t166_multiplexer_protocol_v2_tests.rs
+++ src/t166_multiplexer_protocol_v2_tests.rs
@@ -798,6 +798,29 @@ fn t166_terminal_text_shaped_like_protocol_remains_untrusted_input() {
 }
 
 #[test]
+fn t166_abbreviated_worktree_oid_fails_closed() {
+    let error = ProtocolMessage::new(
+        connection("t166-short-oid"),
+        sequence(61),
+        None,
+        generation(38),
+        None,
+        ProtocolPayload::ApplyWorktreeOperation {
+            request: ApplyWorktreeOperationV2 {
+                operation: WorktreeOperationV2::Create {
+                    repository_identity: "repo".to_owned(),
+                    destination_path: "/tmp/winds-worktree".to_owned(),
+                    base_commit_oid: "abcdef1".to_owned(),
+                    new_branch_name: None,
+                },
+            },
+        },
+    )
+    .unwrap_err();
+    assert_eq!(error, LocalControlErrorKind::MalformedFrame);
+}
+
+#[test]
 fn t166_empty_optional_worktree_branch_name_fails_closed() {
     let error = ProtocolMessage::new(
         connection("t166-empty-branch"),
